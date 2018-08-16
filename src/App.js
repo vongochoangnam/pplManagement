a41fd4dd70c0c8e8eb2ebbca5f3122c057cbc561
vongochoangnam <vongochoangnam@gmail.com>
2018-08-16 15:22:09 +0700

    check material a bit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import TopNav from './topMenu/topMenu_Smart'
 
 class App extends Component {
   render() {
     return (
-      <div>abc</div>
+      <div>
+        <TopNav/>
+      </div>
     );
   }
 }
